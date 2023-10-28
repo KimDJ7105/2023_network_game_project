813927c120a9b451002a563ff7263828f76a3e95
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2023-10-28 21:10:26 +0900

    임시 프로토콜.h 업데이트 - 패킷 이름 소문자로 변경

--- 임시 프로토콜.h
+++ 임시 프로토콜.h
@@ -28,41 +28,41 @@ constexpr int CS_PLAYER_SHOT = 1;
 #pragma pack (push, 1)
 
 //Server to client
-struct SC_CREATE_OBJECT_PACKET{
+struct sc_create_object_packet{
 	int packet_type;
 	int object_type;
 	float pos; //must changed	
 };
 
-struct SC_MOVE_OBJECT_PACKET {
+struct sc_move_object_packet {
 	int packet_type;
 	int object_id;
 	float pos; //must changed
 };
 
-struct SC_DELETE_OBJECT_PACKET {
+struct sc_delete_obejct_packet {
 	int packet_type;
 	int object_id;
 };
 
-struct SC_COLLISION_OBJECT_PACKET {
+struct sc_collision_object_packet {
 	int packet_type;
 	int object_id1;
 	int object_id2;
 };
 
-struct SC_PLAYER_ROLE_PACKET {
+struct sc_player_role_packet {
 	int packet_type;
 	int role;
 }
 
 //client to server
-struct CS_PLAYER_MOVE_PACKET {
+struct cs_player_move_packet {
 	int packet_type;
 	int pressed_key; // must changed
 };
 
-struct CS_PLAYER_SHOT_PACKET {
+struct cs_player_shot_packet {
 	int packet_type;
 	float degree; //must changed
 }
