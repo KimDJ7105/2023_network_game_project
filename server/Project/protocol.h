1049b08dbd6560838f4bc77cb510882cd0e4bb08
Lim Yoon Su <nongbunongbu@gmail.com>
2023-11-08 12:56:17 +0900

    Update protocol.h
    패킷 구조체 및 상수 추가

--- server/Project/protocol.h
+++ server/Project/protocol.h
@@ -2,8 +2,54 @@
 
 constexpr int SERVERPORT = 9000;
 
+// Object ID
+constexpr int PLAYER = 0;
+constexpr int BULLET = 1;
+
+// Key ID
+constexpr int KEY_UP = 0;
+constexpr int KEY_DOWN = 1;
+constexpr int KEY_LEFT = 2;
+
+// Packet ID
+constexpr int SC_CREATE_OBJECT = 0;
+constexpr int SC_MOVE_OBJECT = 1;
+constexpr int SC_DELETE_OBJECT = 2;
+constexpr int SC_COLLISION_OBJECT = 3;
+
 
 #pragma pack(push, 1)
-//여기에 패킷 구조체 정의하기
+
+// 패킷 구조체
+struct sc_parent_packet {
+	int packet_type;
+};
+
+struct sc_create_object_packet : public sc_parent_packet {
+	int object_type;
+	XMVECTOR pos;
+};
+
+struct sc_move_object_packet : public sc_parent_packet {
+	int object_id;
+	XMVECTOR pos;
+};
+
+struct sc_delete_object_packet : public sc_parent_packet {
+	int object_id;
+};
+
+struct sc_player_role_packet : public sc_parent_packet {
+	int role;
+};
+
+struct cs_player_move_packet : public sc_parent_packet {
+	int input_event;
+};
+
+struct sc_collision_object_packet : public sc_parent_packet {
+	int object_id1;
+	int object_id2;
+};
 
 #pragma pack(pop)
