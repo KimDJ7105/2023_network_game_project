fbcb120ff110ae83e0be4428be4577c1fcc12473
Seo Dong Woo <ehddn917@gmail.com>
2023-11-15 12:21:22 +0900

    Protocol 수정
    각 Packet의 기본 생성자 추가

--- server/Project/protocol.h
+++ server/Project/protocol.h
@@ -28,30 +28,42 @@ constexpr int CS_PLAYER_INPUT = 4;
 //s -> c
 struct sc_parent_packet {
 	int packet_type;
+	sc_parent_packet(int type) : packet_type(type) {}
 };
 
 struct sc_create_object_packet : public sc_parent_packet {
 	int object_type;
-	XMVECTOR pos;
+	XMFLOAT4X4 matrix;
+
+	sc_create_object_packet() : sc_parent_packet(0) { object_type = -1; matrix = Matrix4x4::Identity(); }
+	sc_create_object_packet(int type, XMFLOAT4X4 _matrix) : sc_parent_packet(0), object_type(type), matrix(_matrix){}
 };
 
 struct sc_object_transform_packet : public sc_parent_packet {
 	int object_id;
-	XMMATRIX matrix;
+	XMFLOAT4X4 matrix;
+
+	sc_object_transform_packet() : sc_parent_packet(1){ object_id = -1; matrix = Matrix4x4::Identity(); }
 };
 
 struct sc_delete_object_packet : public sc_parent_packet {
 	int object_id;
+
+	sc_delete_object_packet(int id = -1) : sc_parent_packet(2), object_id(id){}
 };
 
 struct sc_collision_object_packet : public sc_parent_packet {
 	int object_id1;
 	int object_id2;
+
+	sc_collision_object_packet() : sc_parent_packet(3){}
 };
 
 //c -> s
 struct cs_player_input_packet : public sc_parent_packet {
 	int input_event;
+
+	cs_player_input_packet() : sc_parent_packet(4){}
 };
 
 
